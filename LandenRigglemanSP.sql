c31c5cfc86135f4ee5a67807c16d20a5698b96d8
LandenRiggleman <159197111+LandenRiggleman@users.noreply.github.com>
2024-10-02 20:32:01 -0400

    Update LandenRigglemanSP.sql
    Fixed a mistake

--- LandenRigglemanSP.sql
+++ LandenRigglemanSP.sql
@@ -10,13 +10,12 @@ create procedure CreateUserProfile
     @Address NVARCHAR(255)
 as
 begin
-   
     if 
     exists (select 1 from users where Email = @Email)
     begin
         Print 'User already exists, please try a new email or login to your existing account'
+    end
     else    
-    
     insert into users (FirstName, LastName, Email, Password, ContactNumber, Address)
     values (@FirstName, @LastName, @Email, @Password, @ContactNumber, @Address);
 end
